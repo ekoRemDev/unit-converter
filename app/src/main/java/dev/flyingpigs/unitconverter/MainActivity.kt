0a2e8d0ff28f2e9034b7d9f4a367c0658c15da97
Ekrem Moralioglu <ekoremdev@gmail.com>
2024-04-26 00:19:35 +0400

    button and showing the toast

--- app/src/main/java/dev/flyingpigs/unitconverter/MainActivity.kt
+++ app/src/main/java/dev/flyingpigs/unitconverter/MainActivity.kt
@@ -1,16 +1,20 @@
 package dev.flyingpigs.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import dev.flyingpigs.unitconverter.ui.theme.UnitConverterTheme
 
@@ -32,29 +36,27 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Composable
-fun UnitConverter(){
+fun UnitConverter() {
     Column {
-        Greeting("Ekrem")
-        Greeting("Ekrem")
+        Text("Unit converter")
+        OutlinedTextField(value = "Enter value", onValueChange = {
+            // Here goes what should happen when text is changed...
+            println("Test is changed")
+        })
         Row {
-            Greeting("Ekrem")
-            Greeting("Ekrem")
+            val context = LocalContext.current
+            Button(onClick = {
+                Toast.makeText(context, "Thanks for Clicking", Toast.LENGTH_LONG).show()
+            }) {
+                Text("Click Me")
+            }
         }
+        Text("Result:")
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterTheme {
-        Greeting("Android")
-    }
+fun UnitConverterPreview() {
+    UnitConverter()
 }
